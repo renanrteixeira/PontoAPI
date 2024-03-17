a9bd7c909614c3e8b25f5e120ee68a583bfd0252
Renan Teixeira <46662928+renanrteixeira@users.noreply.github.com>
2024-03-17 10:10:44 -0300

    Removendo annotations

--- PontoAPI.Core/Entities/Hour.cs
+++ PontoAPI.Core/Entities/Hour.cs
@@ -5,27 +5,16 @@ namespace PontoAPI.Core.Entities
 {
     public class Hour : BaseId
     {
-        [Required]
         public int EmployeeId { get; set; }
-        [Required]
         public DateOnly Date { get; set; }
-        [Required]
         public TypeHour Type { get; set; }  //Gerar enumerable
-        [Required]
         public int TypeDateId { get; set; }
-        [Required]
         public TimeSpan Hour1 { get; set; }
-        [Required]
         public TimeSpan Hour2 { get; set; }
-        [Required]
         public TimeSpan Hour3 { get; set; }
-        [Required]
         public TimeSpan Hour4 { get; set; }
-        [Required]
         public TimeSpan Hour5 { get; set; }
-        [Required]
         public TimeSpan Hour6 { get; set; }
-        [Required]
         public TimeSpan Balance { get; set; }
 
         public TypeDate? TypeDate { get; set; }
